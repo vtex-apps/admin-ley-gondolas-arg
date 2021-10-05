354517b3ec0f34b886980b1b6519841043b15880
Your Name <bonacchigerman@gmail.com>
2021-10-05 10:48:18 -0300

    add node, resolvers + clients

---
+++ node/clients/masterDataClient.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+
+import type { InstanceOptions, IOContext } from '@vtex/api'
+import { ExternalClient } from '@vtex/api'
+
+export default class MasterDataClient extends ExternalClient {
+  constructor(context: IOContext, options?: InstanceOptions) {
+    super(
+      `http://${context.account}.vtexcommercestable.com.br/api/dataentities`,
+      context,
+      {
+        ...options,
+        headers: {
+          VtexIdClientAutCookie:
+            context.adminUserAuthToken ??
+            context.storeUserAuthToken ??
+            context.authToken,
+        },
+      }
+    )
+  }
+
+  public async getData() {
+    return this.http.getRaw(`/leyGondolas/search?_fields=_all`)
+  }
+
+  public async createDocument(body: SaveDataInMasterDataBody) {
+    return this.http.postRaw(
+      `/leyGondolas/documents?_schema=ley-gondolas`,
+      body
+    )
+  }
+
+  public async updateDocument(id: string, body: SaveDataInMasterDataBody) {
+    await this.http.patch(
+      `/leyGondolas/documents/${id}?_schema=ley-gondolas`,
+      body
+    )
+
+    return { status: 200 }
+  }
+}
--- react/LeyGondolas.tsx
+++ react/LeyGondolas.tsx
@@ -8,33 +8,84 @@ import CategoriesTable from './components/CategoriesTable'
 
 export default function LeyGondolas() {
   const intl = useIntl()
-  const categoriesList: CategoriesRow[] = [
-    {
-      categorie: 'Leches',
-      categorieCatalog: '',
-      bestLowerProduct: '',
-    },
-    {
-      categorie: 'Cremas de leche, mantecas y margarinas',
-      categorieCatalog: '',
-      bestLowerProduct: '',
-    },
-    {
-      categorie: 'Quesos untables y ricotas',
-      categorieCatalog: '',
-      bestLowerProduct: '',
-    },
-    {
-      categorie: 'Postres lácteos y yogures',
-      categorieCatalog: '',
-      bestLowerProduct: '',
-    },
-    {
-      categorie: 'Masas frescas y levaduras',
-      categorieCatalog: '',
-      bestLowerProduct: '',
-    },
+
+  const listOfLawCategories: string[] = [
+    'Leches',
+    'Cremas de leche, mantecas y margarinas',
+    'Quesos untables y ricotas',
+    'Postres lácteos y yogures',
+    'Masas frescas y levaduras',
+    'Fiambres, embutidos y encurtidos',
+    'Quesos excepto untables y ricotas',
+    'Hamburguesas, milanesas y bocaditos de carne, pollo, cerdo y/o pescado congelados',
+    'Vegetales congelados, milanesas y medallones sin carne congelados',
+    'Comidas preparadas y panificados congelados',
+    'Helados y postres congelados',
+    'Frutas y verduras',
+    'Salsas y aderezos',
+    'Especias y condimentos',
+    'Polvo para postres y repostería, y productos de repostería',
+    'Conservas y salsas de tomate',
+    'Otras conservas',
+    'Pastas secas',
+    'Arroz',
+    'Sopas, caldos y puré',
+    'Legumbres secas, otras harinas, granos y semillas',
+    'Aceites',
+    'Panes rallados y rebozadores',
+    'Harinas de trigo y premezclas',
+    'Golosinas, alfajores y chocolates',
+    'Panificados y cereales',
+    'Galletitas',
+    'Infusiones',
+    'Mermeladas, dulces y miel',
+    'Dulce de leche',
+    'Endulzantes',
+    'Snacks',
+    'Jugos',
+    'Aguas saborizadas',
+    'Gaseosas',
+    'Aguas',
+    'Bebidas sin alcohol refrigeradas',
+    'Cervezas y aperitivos',
+    'Vinos y espumantes',
+    'Espirituosas, destilados y licores',
+    'Bebidas con alcohol refrigeradas',
+    'Afeitado y depilación',
+    'Colonias y desodorantes corporales, polvos pédicos y talcos',
+    'Jabones de tocador, geles y sales de baño, artículos de baño',
+    'Cremas decolorantes y coloración para el cabello',
+    'Champúes, Acondicionadores, fijadores y productos para el tratamiento capilar',
+    'Cuidado facial y artículos de farmacia',
+    'Cremas corporales',
+    'Protectores solares y bronceadores',
+    'Cuidado oral',
+    'Productos para la higiene del bebé, accesorios para el bebé y alimentación infantil',
+    'Pañales y ropa interior descartable',
+    'Productos de gestión menstrual, toallas para la incontinencia y cuidado materno',
+    'Accesorios de limpieza',
+    'Accesorios de cocina',
+    'Jabón en pan, prelavado, quitamanchas y perfumes para la ropa',
+    'Jabones en polvo, jabones líquidos y suavizantes para la ropa',
+    'Desodorantes y desinfectantes ambientales',
+    'Lavandinas',
+    'Lavavajillas',
+    'Limpieza de pisos y muebles',
+    'Limpiadores cremosos, de cocina, de baño y multiuso',
+    'Insecticidas y repelentes',
+    'Papeles',
+    'Alimentos y accesorios para mascotas',
   ]
+
+  const categoriesList: CategoriesRow[] = listOfLawCategories.map(
+    (lawCategorie: string) => {
+      return {
+        categorie: lawCategorie,
+        categorieCatalog: '',
+        bestLowerProduct: '',
+      }
+    }
+  )
 
   return (
     <Layout
